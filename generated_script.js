b9e0d85f9f0afb955e35a469a502fac735bc67b7
Simon Doksrød <simon.doksrod@outlook.com>
2020-06-11 21:59:16 +0200

    initial

---
+++ generated_script.js
@@ -0,0 +1,105 @@
+var DevLogIDs = ["t200610"];
+
+var maximized_log = null;
+
+function SetLogState(log_id, should_maximize)
+{
+    var state_string = (should_maximize ? "maximized" : "minimized");
+
+    var log_objects = document.getElementsByClassName(log_id)
+    if (log_objects === undefined || log_objects.length === 0)
+    {
+        console.log("The dev log (id: {0}) is missing the title, date and content objects", log_id);
+    }
+
+    else
+    {
+        for (var i = 0; i < log_objects.length; ++i)
+        {
+            if (log_objects[i].className.includes("log_content_minimized") ||
+                log_objects[i].className.includes("log_content_maximized"))
+            {
+                log_objects[i].className = log_id.concat(" log_content_".concat(state_string));
+            }
+
+            else if (!(log_objects[i].className.includes("log_title") ||
+                       log_objects[i].className.includes("log_date")))
+            {
+                console.log("The dev log (id: {0}) is missing the log_*_* class", log_id);
+            }
+        }
+    }
+}
+
+function SwitchTab()
+{
+	var current_tab = window.location.hash;
+
+	about = document.getElementById("about_tab");
+	docs  = document.getElementById("docs_tab");
+	log   = document.getElementById("log_tab");
+
+	if (current_tab === "#docs")
+	{
+		about.style.display = "none";
+		docs.style.display  = "block";
+		log.style.display   = "none";
+	}
+
+	else if (current_tab.startsWith("#log"))
+	{
+		about.style.display = "none";
+		docs.style.display  = "none";
+		log.style.display   = "block";
+	}
+
+	else
+	{
+		window.location.hash = "#about";
+		about.style.display  = "block";
+		docs.style.display   = "none";
+		log.style.display    = "none";
+	}
+
+	if (current_tab.startsWith("#log@"))
+	{
+		var log_id = current_tab.slice("#log@".length);
+
+        if (DevLogIDs.find(x => x === log_id) === undefined)
+        {
+            window.location.hash = "#log";
+
+            if (maximized_log != null)
+            {
+                SetLogState(maximized_log, false);
+                maximized_log = null;
+            }
+        }
+
+        else
+        {
+            if (maximized_log != null)
+            {
+                SetLogState(maximized_log, false);
+            }
+
+            maximized_log = log_id;
+            SetLogState(log_id, true);
+        }
+	}
+
+    else if (maximized_log != null)
+    {
+        SetLogState(maximized_log, false);
+        maximized_log = null;
+    }
+}
+
+document.addEventListener("readystatechange", event => {
+	if (event.target.readyState === "complete")
+	{
+		SwitchTab();
+	}
+});
+
+window.addEventListener("hashchange", SwitchTab);
