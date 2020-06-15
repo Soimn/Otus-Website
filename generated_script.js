92c66e215097f41bcaad788fd2bf14ce8ec36aa0
Simon Doksrød <simon.doksrod@outlook.com>
2020-06-15 17:41:04 +0200

    initial post

--- generated_script.js
+++ generated_script.js
@@ -1,43 +1,54 @@
-var DevLogIDs = ["t200610"];
+var DevLogIDs = ["maed200614"];
 
 var maximized_log = null;
 
 function SetLogState(log_id, should_maximize)
 {
-    var state_string = (should_maximize ? "maximized" : "minimized");
+	var state_string = (should_maximize ? "maximized" : "minimized");
 
-    var log_objects = document.getElementsByClassName(log_id)
-    if (log_objects === undefined || log_objects.length === 0)
-    {
-        console.log("The dev log (id: {0}) is missing the title, date and content objects", log_id);
-    }
+	var log_objects = document.getElementsByClassName(log_id)
+	if (log_objects === undefined || log_objects.length === 0)
+	{
+		console.log("The dev log (id: {0}) is missing the title, date and content objects", log_id);
+	}
 
-    else
-    {
-        for (var i = 0; i < log_objects.length; ++i)
-        {
-            if (log_objects[i].className.includes("log_content_minimized") ||
-                log_objects[i].className.includes("log_content_maximized"))
-            {
-                log_objects[i].className = log_id.concat(" log_content_".concat(state_string));
-            }
+	else
+	{
+		for (var i = 0; i < log_objects.length; ++i)
+		{
+			if (log_objects[i].className.includes("log_title_minimized") ||
+			    log_objects[i].className.includes("log_title_maximized"))
+			{
+				log_objects[i].className = log_id.concat(" log_title_".concat(state_string));
+			}
 
-            else if (!(log_objects[i].className.includes("log_title") ||
-                       log_objects[i].className.includes("log_date")))
-            {
-                console.log("The dev log (id: {0}) is missing the log_*_* class", log_id);
-            }
-        }
-    }
+			else if (log_objects[i].className.includes("log_date_minimized") ||
+				 log_objects[i].className.includes("log_date_maximized"))
+			{
+				log_objects[i].className = log_id.concat(" log_date_".concat(state_string));
+			}
+
+			else if (log_objects[i].className.includes("log_content_minimized") ||
+				 log_objects[i].className.includes("log_content_maximized"))
+			{
+				log_objects[i].className = log_id.concat(" log_content_".concat(state_string));
+			}
+
+			else
+			{
+				console.log("The dev log (id: {0}) is missing the log_* class", log_id);
+			}
+		}
+	}
 }
 
 function SwitchTab()
 {
 	var current_tab = window.location.hash;
 
-	about = document.getElementById("about_tab");
-	docs  = document.getElementById("docs_tab");
-	log   = document.getElementById("log_tab");
+	var about = document.getElementById("about_tab");
+	var docs  = document.getElementById("docs_tab");
+	var log   = document.getElementById("log_tab");
 
 	if (current_tab === "#docs")
 	{
@@ -65,51 +76,81 @@ function SwitchTab()
 	{
 		var log_id = current_tab.slice("#log@".length);
 
-        if (DevLogIDs.find(x => x === log_id) === undefined)
-        {
-            window.location.hash = "#log";
+		if (DevLogIDs.find(x => x === log_id) === undefined)
+		{
+			window.location.hash = "#log";
 
-            if (maximized_log != null)
-            {
-                SetLogState(maximized_log, false);
-                maximized_log = null;
-            }
-        }
+			if (maximized_log != null)
+			{
+				SetLogState(maximized_log, false);
+				maximized_log = null;
+			}
+		}
 
-        else
-        {
-            if (maximized_log != null)
-            {
-                SetLogState(maximized_log, false);
-            }
+		else
+		{
+			if (maximized_log != null)
+			{
+				SetLogState(maximized_log, false);
+			}
 
-            maximized_log = log_id;
-            SetLogState(log_id, true);
-        }
+			maximized_log = log_id;
+			SetLogState(log_id, true);
+		}
 	}
 
-    else if (maximized_log != null)
-    {
-        SetLogState(maximized_log, false);
-        maximized_log = null;
-    }
+	else if (maximized_log != null)
+	{
+		window.location.hash = "#log";
+		SetLogState(maximized_log, false);
+		maximized_log = null;
+	}
 
-    window.scrollTo(0, 0);
+	if (current_tab.startsWith("#log"))
+	{
+		if (maximized_log != null)
+		{
+			for (title of document.getElementsByClassName("log_title_minimized")) title.style.display = "none";
+			for (date  of document.getElementsByClassName("log_date_minimized"))  date.style.display  = "none";
+		}
+
+		else
+		{
+			for (title of document.getElementsByClassName("log_title_minimized")) title.style.display = "block";
+			for (date  of document.getElementsByClassName("log_date_minimized"))  date.style.display  = "block";
+		}
+	}
+
+	window.scrollTo(0, 0);
 }
 
 document.addEventListener("readystatechange", event => {
 	if (event.target.readyState === "complete")
 	{
-        if (navigator.userAgent.match(/(iphone)|(ipad)/i))
-        {
-            var view = document.querySelector("meta[name=\"viewport\"]");
+		if (navigator.userAgent.match(/(iphone)|(ipad)/i))
+		{
+			var view = document.querySelector("meta[name=\"viewport\"]");
 
-            if (view)
-            {
-                view.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0";
-                window.addEventListener("gesturestart", function() {});
-            }
-        }
+			if (view)
+			{
+				view.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0";
+				window.addEventListener("gesturestart", function() {});
+			}
+		}
+
+		var log_tab = document.getElementById("log_tab");
+
+		var log_entries = [];
+		for (child of log_tab.children) log_entries.push(child);
+
+		log_entries.sort(function (a, b) {
+			var a_date = a.className.toString().replace(/[^0-9]/g, "");
+			var b_date = b.className.toString().replace(/[^0-9]/g, "");
+
+			return (a_date > b_date ? -1 : (b_date > a_date ? 1 : 0));
+		});
+
+		for (entry of log_entries) log_tab.appendChild(entry);
 
 		SwitchTab();
 	}
